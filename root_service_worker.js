cd5ced00041816ada33d89015f41c0ba8233d663
Nsikan <nsikane@live.co.uk>
2018-06-19 22:47:20 +0100

    Feat: Add service worker install and fetch events

--- root_service_worker.js
+++ root_service_worker.js
@@ -1,5 +1,31 @@
 /* eslint linebreak-style: ["error", "windows"] */
 /* global self */
+/* global caches */
+/* global fetch */
+
+self.addEventListener('install', (event) => {
+  const imgUrlsToAdd = Array(10).fill('').map((each, index) => `img/${index + 1}.jpg`);
+  const urlsToCache = [
+    'css/styles.css',
+    'js/dbhelper.js',
+    'js/main.js',
+    'js/restaurant_info.js',
+  ].concat(imgUrlsToAdd);
+  event.waitUntil(caches.open('mws-restaurant-v2')
+    .then(cache => cache.addAll(urlsToCache))
+    .then(() => console.log('assets fully cached'))
+    .catch(err => console.log(`cache failed to open because of: ${err}`)));
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('service worker activating');
+});
+
 self.addEventListener('fetch', (event) => {
   console.log(event.request);
+  return event.respondWith(caches.match(event.request)
+    .then((response) => {
+      if (response) return response;
+      return fetch(event.request);
+    }));
 });
