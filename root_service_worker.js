95fab9e65ec14e4edc9021abecd97180b90b6097
Nsikan <nsikane@live.co.uk>
2018-06-20 09:08:34 +0100

    Feat: Render visited pages when offline

--- root_service_worker.js
+++ root_service_worker.js
@@ -6,26 +6,34 @@
 self.addEventListener('install', (event) => {
   const imgUrlsToAdd = Array(10).fill('').map((each, index) => `img/${index + 1}.jpg`);
   const urlsToCache = [
+    '/',
+    'data/restaurants.json',
     'css/styles.css',
     'js/dbhelper.js',
     'js/main.js',
     'js/restaurant_info.js',
   ].concat(imgUrlsToAdd);
-  event.waitUntil(caches.open('mws-restaurant-v2')
+  event.waitUntil(caches.open('mws-restaurant-v5')
     .then(cache => cache.addAll(urlsToCache))
     .then(() => console.log('assets fully cached'))
     .catch(err => console.log(`cache failed to open because of: ${err}`)));
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', () => {
   console.log('service worker activating');
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-  return event.respondWith(caches.match(event.request)
-    .then((response) => {
-      if (response) return response;
-      return fetch(event.request);
-    }));
+  event.respondWith(
+    caches.open('mws-restaurant-v5')
+      .then(cache => cache.match(event.request))
+      .then((res) => {
+        if (res) return res;
+        return fetch(event.request).then(response =>
+          caches.open('mws-restaurant-v5')
+            .then(cache => cache.put(event.request, response.clone()))
+            .then(() => response));
+      })
+      .catch(err => console.log(`fetch operation failed because of: ${err}`))
+  );
 });
