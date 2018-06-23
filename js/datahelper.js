84364a87c0cad24cf33bc930b8f4e6a8003997b3
Nsikan <nsikane@live.co.uk>
2018-06-23 13:12:01 +0100

    Feat: FetchByCuisine Promise Method

--- js/datahelper.js
+++ js/datahelper.js
@@ -20,7 +20,7 @@ class DataHelper {
   static fetchRestaurants() {
     return self.fetch(DataHelper.SERVER_URL)
       .then((response) => {
-        if (!response.ok) throw (new Error('response was not a 200'));
+        if (!response.ok) throw (new Error(`response was not a 200, because ${response.body}`));
         return response.json();
       })
       .catch(err => console.log(`the data could not be fetched because of ${err}`));
@@ -43,6 +43,21 @@ class DataHelper {
   }
 
   /**
+   * Fetch restaurants by a cuisine type with proper error handling.
+   */
+  static fetchRestaurantByCuisine(cuisine) {
+    // Fetch all restaurants  with proper error handling
+    return DataHelper.fetchRestaurants()
+      .then(_restaurants =>
+      // Filter restaurants to have only given cuisine type
+        _restaurants.filter(r => r.cuisine_type === cuisine))
+      .catch((err) => {
+        console.error(`the data could not be fetched because of ${err}`);
+        return null;
+      });
+  }
+
+  /**
    * Fetch all neighborhoods with proper error handling.
    */
   static fetchNeighborhoods() {
@@ -52,6 +67,22 @@ class DataHelper {
         const neighborhoods = restaurants.map((v, i) => restaurants[i].neighborhood);
         const uniqueNeighborhoods = neighborhoods.filter((v, i) => neighborhoods.indexOf(v) === i);
         return uniqueNeighborhoods;
+      })
+      .catch(err => `there was an error fetching the neighbourhoods: ${err}`);
+  }
+
+  /**
+   * Fetch all cuisines with proper error handling.
+   */
+  static fetchCuisines() {
+    // Fetch all restaurants
+    return DataHelper.fetchRestaurants()
+      .then((restaurants) => {
+        // Get all cuisines from all restaurants
+        const cuisines = restaurants.map((v, i) => restaurants[i].cuisine_type);
+        // Remove duplicates from cuisines
+        const uniqueCuisines = cuisines.filter((v, i) => cuisines.indexOf(v) === i);
+        return uniqueCuisines;
       })
       .catch(err => `there was an error fetching the neighbourhoods: ${err}`);
   }
