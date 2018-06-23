405aa223d4707489880e1e70a787a99873a63c26
Nsikan <nsikane@live.co.uk>
2018-06-23 19:47:18 +0100

    Fix: image extension for restaurant images

--- js/datahelper.js
+++ js/datahelper.js
@@ -136,7 +136,7 @@ class DataHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`/img/${restaurant.photograph}.jpg`);
   }
 
   /**
