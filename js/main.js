e6d44b94752a88a18b9671ffe7eb21c208aac1e6
Nsikan <nsikane@live.co.uk>
2018-07-06 09:05:11 +0100

    Feat: Add aria label to uniquely identify each restaurant for AT

--- js/main.js
+++ js/main.js
@@ -157,6 +157,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DataHelper.urlForRestaurant(restaurant);
+  more.setAttribute('aria-label', `View Details for ${restaurant.name}`);
   li.append(more);
 
   return li;
