f933c0fe8b6c808c2ccbed7df0abadc8866ac980
Nsikan <nsikane@live.co.uk>
2018-08-03 16:06:20 +0100

    Fix: Add fallback image name for restaurants

--- js/datahelper.js
+++ js/datahelper.js
@@ -146,7 +146,7 @@ class DataHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}.jpg`);
+    return (`/img/${restaurant.photograph || restaurant.id}.jpg`);
   }
 
   /**
