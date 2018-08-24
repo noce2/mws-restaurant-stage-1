b50e2fd56289b72bcffd835cea178cc7f5ce73b5
Nsikan <nsikane@live.co.uk>
2018-08-24 18:00:18 +0100

    Fix: Allow POST reviews to cache in IDB

--- root_service_worker.js
+++ root_service_worker.js
@@ -42,18 +42,29 @@ self.addEventListener('fetch', (event) => {
     if (!self.navigator.onLine) {
       console.log('browser is offline so caching post request');
       console.log(event);
-      event.respondWith(IndexedDBStore
-        /* the request object needs to be stringified because it has methods attached to it
-         * which cause the put operation to fail. Stringification removes the methods and
-         * parsing turns the string into an object once more. */
-        .put(null, JSON.parse(JSON.stringify(event.request)), IndexedDBStore.queueForNewReviews)
-        .then((result) => {
-          console.log(`result from cache operation is ${result}`);
-          return new Response('', {
-            status: 202,
-            statusText: 'The request has been accepted but the resource is unavailable',
-          });
-        }));
+      event.respondWith(
+        event.request.clone().text()
+          .then(postBody => IndexedDBStore
+          /* the request object needs to be stringified because it has methods attached to it
+           * which cause the put operation to fail. Stringification removes the methods and
+           * parsing turns the string into an object once more. */
+            .put(null, {
+              request: {
+                body: postBody,
+                method: event.request.method,
+                url: event.request.url,
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+                } /* allow comma dangle here, it refuses to commit the object otherwise */
+              } /* allow comma dangle here, it refuses to commit the object otherwise */
+            }, IndexedDBStore.queueForNewReviews)
+            .then((result) => {
+              console.log(`result from cache operation is ${result}`);
+              return new Response('', {
+                status: 202,
+                statusText: 'The request has been accepted but the resource is unavailable',
+              });
+            })));
     } else {
       console.log('do nothing because this is a post request');
     }
